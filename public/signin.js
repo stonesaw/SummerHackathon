1d9e315f5b385f1d9977aab61b33507e1a9d1ec1
NeKodai <gangshanhongda@gmail.com>
2020-08-15 21:05:38 +0900

    index.html大幅変更

--- public/signin.js
+++ public/signin.js
@@ -1,14 +1,88 @@
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
+// function onSignIn(googleUser) {
+//     // Useful data for your client-side scripts:
+//     var profile = googleUser.getBasicProfile();
+//     console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+//     console.log('Full Name: ' + profile.getName());
+//     console.log('Given Name: ' + profile.getGivenName());
+//     console.log('Family Name: ' + profile.getFamilyName());
+//     console.log("Image URL: " + profile.getImageUrl());
+//     console.log("Email: " + profile.getEmail());
 
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+//     // The ID token you need to pass to your backend:
+//     var id_token = googleUser.getAuthResponse().id_token;
+//     console.log("ID Token: " + id_token);
+// }
+// Client ID and API key from the Developer Console
+var CLIENT_ID = ENV_CLIENT_ID;
+var API_KEY = ENV_API_KEY;
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events";
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('hasAuthorized_button');
+
+/**
+*  On load, called to load the auth2 library and API client library.
+*/
+function handleClientLoad() {
+
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    }, function (error) {
+        appendPre(JSON.stringify(error, null, 2));
+    });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn().then(function (result) {
+        location.href = "./mylist.html";
+    });
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+    location.href = "./mylist.html";
+    //gapi.auth2.getAuthInstance().signOut();
 }
